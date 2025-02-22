15558a1e1c88d7dc744ebc10abdf6bcfeba4fec5
OumObayid <elobayidoumaima@gmail.com>
2025-02-22 23:57:16 +0100

    suit

--- back-endd/categories/updateCategorie.php
+++ back-endd/categories/updateCategorie.php
@@ -1,45 +1,36 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Methods: PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $conn = mysqli_connect("localhost", "root", "", "flowerShop");
 
 // Gérer les pré-requêtes CORS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Methods: POST');
     exit(0);
 }
-
 
 // Lecture des données envoyées
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Récupérer les données de la requête
 $id = isset($data['id']) ? mysqli_real_escape_string($conn, $data['id']) : null;
 $nom = isset($data['nom']) ? mysqli_real_escape_string($conn, $data['nom']) : null;
 
-// Vérifier si l'ID est manquant
-if (is_null($id)) {
-    echo json_encode(["success" => false, "message" => "ID manquant"]);
+// Vérification des données
+if (is_null($id) || empty($nom)) {
+    echo json_encode(["success" => false, "message" => "ID et nom requis"]);
     exit;
 }
 
-// Construire la requête SQL
-$query = "UPDATE categories SET nom='$nom'";
-
-
-$query .= " WHERE id='$id'";
-
-// Exécuter la requête
+// Requête SQL pour mettre à jour la catégorie
+$query = "UPDATE categories SET nom='$nom' WHERE id='$id'";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
-    echo json_encode(["success" => true,"message"=>"votre catégorie a été bien modifié"]);
+    echo json_encode(["success" => true, "message" => "Catégorie mise à jour"]);
 } else {
     echo json_encode(["success" => false, "message" => mysqli_error($conn)]);
 }
-
 
 mysqli_close($conn);
